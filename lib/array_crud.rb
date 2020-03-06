7bba1da7e41c37164f0ddb5e9606715b61d092af
Ty Raino <mstymetris@yahoo.com>
2020-03-06 21:20:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,10 @@ cars = ["Hyundai", "Lexus", "Mazda", "Kia"]
 end
 
 def add_element_to_end_of_array(array, element)
-array = []
 array << element
 end
 
 def add_element_to_start_of_array(array, element)
-array = []
 array.unshift(element)
 end
 
@@ -21,21 +19,17 @@ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-array = []
 array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-array = []
 array[2]
 end
 
 def retrieve_first_element_from_array(array)
-array = []
 array.first
 end
 
 def retrieve_last_element_from_array(array)
-array= []
 array.last
 end
