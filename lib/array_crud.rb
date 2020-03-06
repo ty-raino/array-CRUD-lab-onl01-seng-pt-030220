512fff805d97ccba18eb3160aea36efc5178a046
Ty Raino <mstymetris@yahoo.com>
2020-03-06 20:57:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-cars = ["Hyundai", "Lexus" "Honda", "Mazda"]
+cars = ["Hyundai", "Lexus" "Honda", "Mazda", "Kia"]
 end
 
 def add_element_to_end_of_array(array, element)
